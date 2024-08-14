f2f64f93de4e2164eaff5d14a638281478607527
wildy13 <wildy.simanjuntak13@gmail.com>
2024-08-14 09:34:13 +0700

    add carousel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/tailwind.css';
 import './css/main.css'
 
 import { SlideOver } from './slideOver/index.js';
+import { Carousel } from './Carousel/index.js';
 
 
-export { SlideOver };
+export { SlideOver, Carousel };
